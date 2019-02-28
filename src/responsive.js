3b07de54e7e909805a00e76aed93219937b249db
Alex <atenefyr@gmail.com>
2019-02-27 20:03:00 -0600

    minify files for efficiency, added gateway chambers in calculator

--- src/responsive.js
+++
@@ -1,8 +0,0 @@
-function expand() {
-    var navBar = document.getElementById("navbar");
-    if (navBar.className === "closed") {
-        navBar.className = "open";
-    } else {
-        navBar.className = "closed";
-    }
-}
