7a680c09fe52508ab83365ebfdc9fecaadab7698
Alex <atenefyr@gmail.com>
2019-04-07 22:20:18 -0500

    small joke

--- src/js/unminified/responsive.js
+++ src/js/unminified/responsive.js
@@ -6,3 +6,11 @@ function expand() {
         navBar.className = "closed";
     }
 }
+
+window.addEventListener("load", function() {
+    if (document.getElementById("total")) {
+        document.getElementById("total").addEventListener("click", function() {
+            if (document.getElementById("total").innerHTML === "811.12") document.body.style.fontFamily = '"Comic Sans MS", cursive, sans-serif';
+        });
+    }
+});
