92aad1033d91c379be942ea9f043a3b4acd21367
Alex <atenefyr@gmail.com>
2019-05-05 14:23:04 -0500

    EFT button

--- src/js/unminified/responsive.js
+++ src/js/unminified/responsive.js
@@ -1,3 +1,5 @@
+var isEFT = false;
+
 function expand() {
     var navBar = document.getElementById("navbar");
     if (navBar.className === "closed") {
@@ -7,10 +9,23 @@ function expand() {
     }
 }
 
-window.addEventListener("load", function() {
-    if (document.getElementById("total")) {
-        document.getElementById("total").addEventListener("click", function() {
-            if (document.getElementById("total").innerHTML === "811.12") document.body.style.fontFamily = '"Comic Sans MS", cursive, sans-serif';
-        });
+function eft() {
+    isEFT = !isEFT;
+    var newDisplayEFT = "none";
+    var newDisplayNOT = "inline-block";
+    if (isEFT) {
+        document.getElementById("header_text").innerHTML += " (EFT)";
+        newDisplayEFT = "inline-block";
+        newDisplayNOT = "none";
+    } else {
+        document.getElementById("header_text").innerHTML = document.getElementById("header_text").innerHTML.replace(" (EFT)", "");
     }
-});
+    var elems = document.getElementsByClassName("eft");
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].style.display = newDisplayEFT;
+    }
+    elems = document.getElementsByClassName("not_eft");
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].style.display = newDisplayNOT;
+    }
+}
