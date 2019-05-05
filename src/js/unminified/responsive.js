7822fcfe5a92cd2392da41bb4f5f25a0c202b275
Alex <atenefyr@gmail.com>
2019-05-05 14:58:25 -0500

    add noscript warning

--- src/js/unminified/responsive.js
+++ src/js/unminified/responsive.js
@@ -1,4 +1,7 @@
+"use strict";
+
 var isEFT = false;
+var HEADER_DATA = " (<abbr title=\"EFT, or &quot;Exo Flight Test,&quot; is Astroneer's system for opt-in open betas for the next content update. With this option enabled, this calculator will show content exclusive to the Exo Flight Test and not present in the latest stable release.\">EFT</abbr>)";
 
 function expand() {
     var navBar = document.getElementById("navbar");
@@ -14,11 +17,11 @@ function eft() {
     var newDisplayEFT = "none";
     var newDisplayNOT = "inline-block";
     if (isEFT) {
-        document.getElementById("header_text").innerHTML += " (EFT)";
+        document.getElementById("header_text").innerHTML += HEADER_DATA;
         newDisplayEFT = "inline-block";
         newDisplayNOT = "none";
     } else {
-        document.getElementById("header_text").innerHTML = document.getElementById("header_text").innerHTML.replace(" (EFT)", "");
+        document.getElementById("header_text").innerHTML = document.getElementById("header_text").innerHTML.replace(HEADER_DATA, "");
     }
     var elems = document.getElementsByClassName("eft");
     for (var i = 0; i < elems.length; i++) {
@@ -29,3 +32,7 @@ function eft() {
         elems[i].style.display = newDisplayNOT;
     }
 }
+
+window.addEventListener("load", function() {
+    document.getElementById("content").style.display = "";
+});
